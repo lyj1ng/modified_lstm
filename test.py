09cd5eab5d09ce478a4aa9fc2c3abf4d1bbc9b9c
forev <yjli98@qq.com>
2021-11-08 18:49:53 +0800

    增加了batch，validation，使用三角函数进行lstm的测试

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ class ToyLossLayer:
         return diff
 
 
-def train_lstm(xdata, ydata, epochs=100, batch_size=16):
+def train_lstm(xdata, ydata, epochs=100, batch_size=32, validaion_data=None):
     # learns to repeat simple sequence from random inputs
     np.random.seed(0)
 
@@ -31,7 +31,7 @@ def train_lstm(xdata, ydata, epochs=100, batch_size=16):
 
     for cur_iter in range(epochs):
         print("iter", "%2s" % str(cur_iter), end=": ")
-        for batch in range(int(len(xdata) / batch_size)+1):
+        for batch in range(int(len(xdata) / batch_size) + 1):
             y_list = ydata[batch * batch_size:(batch + 1) * batch_size]
             input_val_arr = xdata[batch * batch_size:(batch + 1) * batch_size]
 
@@ -54,35 +54,63 @@ def train_lstm(xdata, ydata, epochs=100, batch_size=16):
         for ind in range(len(y_list)):
             lstm_net.x_list_add(input_val_arr[ind])
         loss = lstm_net.y_list_is(y_list, ToyLossLayer)
-        print("loss:", "%.3e" % loss)
+        print("training loss:", "%.3e" % loss, end=' ')
         lstm_net.x_list_clear()
+        if validaion_data:
+            validation(lstm_net, validaion_data[0], validaion_data[1])
+    return lstm_net
+
+
+def validation(model, x, y):
+    y_list = y
+    input_val_arr = x
+    for ind in range(len(y_list)):
+        model.x_list_add(input_val_arr[ind])
+    # y_pred = [round(lstm_net.lstm_node_list[ind].state.h[0], 2) for ind in range(len(y_list))]
+    loss = model.y_list_is(y_list, ToyLossLayer)
+    print("validation loss:", "%.3e" % loss)
+    model.x_list_clear()
+
+
+def predict(model, x):
+    for xx in x:
+        model.x_list_add(xx)
+    y_pred = [round(model.lstm_node_list[ind].state.h[0], 2) for ind in range(len(x))]
+    return y_pred
 
 
 if __name__ == "__main__":
-    # example_0()
     import math
 
     i = 0
     list1 = []  # 定义一个空list
-    while (i < 360):
+    while i < 360:
         list1.append(i)  # 把数据增加到列表末
-        i = i + 3.6;  # 因为我要一个周期里有100个点，所以点间距为3.6度
+        i = i + 3.6  # 因为我要一个周期里有100个点，所以点间距为3.6度
     # 以上生成了100个点的角度数据
     list2 = [x * math.pi / 180 for x in list1]  # 把角度转成弧度
-
     list3 = [math.sin(x) for x in list2]  # 求出正弦值,并放大100倍
-
-    list4 = [round(x,2) for x in list3]  # 取整
-
+    list4 = [round(x, 2) for x in list3]  # 取整
     list4 *= 2
+
     y = []
     x = []
-    for i in range(500):
-        start_idx = np.random.randint(1, len(list4)//2)
-        xx = list4[start_idx:start_idx+10]
-        yy = list4[start_idx+11]
+    for i in range(10):
+        start_idx = np.random.randint(1, len(list4) // 2)
+        xx = list4[start_idx:start_idx + 10]
+        yy = list4[start_idx + 11]
         x.append(xx)
         y.append(yy)
+    xTest, yTest = [], []
+    for i in range(10):
+        start_idx = np.random.randint(1, len(list4) // 2)
+        xx = list4[start_idx:start_idx + 10]
+        yy = list4[start_idx + 11]
+        xTest.append(xx)
+        yTest.append(yy)
 
-    train_lstm(x, y, 1000)
-    print(y)
+    lstmNet = train_lstm(x, y, epochs=2000, batch_size=64, validaion_data=(xTest, yTest))
+    # validation(lstmNet, xTest, yTest)
+    result = predict(lstmNet, xTest)
+    print(result, '\n', yTest)
+    # print(y)
